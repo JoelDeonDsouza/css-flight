6bb66ec49792ae0ca782f58bd866631a572ff3e2
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-10-31 15:07:59 +0100

    final code commit -m

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+// !img import
+import plane from "./img/plane.png";
+import cloud from "./img/cloud.png";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <body>
+      <section className="container">
+        <div className="clouds">
+          <img src={cloud} alt="cloud" className="--i:1;" />
+          <img src={cloud} alt="cloud" className="--i:2;" />
+          <img src={cloud} alt="cloud" className="--i:3;" />
+        </div>
+        <div className="clouds cloudsCopy">
+          <img src={cloud} alt="cloud" className="--i:1;" />
+          <img src={cloud} alt="cloud" className="--i:2;" />
+          <img src={cloud} alt="cloud" className="--i:3;" />
+        </div>
+        <div className="runway"></div>
+        <img src={plane} alt="plane" className="plane" />
+      </section>
+    </body>
+  );
 }
 
 export default App;
